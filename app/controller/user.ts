0595c020d1c0215d64964748d1cb586023c9aa61
Andy <659168590@qq.com>
2018-07-31 19:14:12 +0800

    chores

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -1,7 +1,7 @@
 import BaseController from './base'
 import { Get, Prefix } from '../../lib';
 
-@Prefix('/api/user', 'UserController')
+Prefix('/api/user', 'UserController')
 export default class UserController extends BaseController {
   
   @Get('/test')
